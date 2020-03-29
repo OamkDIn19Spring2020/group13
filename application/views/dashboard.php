a548d3df8811e4976dfce111606a7f73f99d02ba
marianthiadm <38660415+marianthiadm@users.noreply.github.com>
2020-03-30 02:25:09 +0300

    Revert "fix exception"

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -13,7 +13,7 @@
                             <span class="icon-bar"></span>
                         </button>
                         <a class="navbar-brand" href="#">
-                            Hello <?= $this->session->userdata('userSessionName'); ?>
+                            Hello <?= $this->session->userdata('userSession'); ?>
                         </a>
                     </div>
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">            
