67d0a55815429dda6632e9e6179be4b1cfcbf106
marianthi <k9amda00@students.oamk.fi>
2020-04-08 18:16:13 +0300

    Update header_adm.php

--- application/views/components-adm/header_adm.php
+++ application/views/components-adm/header_adm.php
@@ -3,8 +3,26 @@
                 <i class="fas fa-tachometer-alt"></i>
                 <span>Group 13</span>
             </div>
-            <a href="#" class="nav-trigger"><span></span></a>
+            <a href="" class="nav-trigger"><span></span></a>
         </div>
+        <div  style="text-align: center;" id = "name">
+         <form action = "" method = "post">
+            <label><p style="color:darkblue;"> First name:</p></label>
+			<input type = "text" name = "First Name" id = "First Name" />
+            <label><p style="color:darkblue;"> Last name:</p></label>
+			<input type = "text" name = "Last Name" id = "Last Name" />
+            <label><p style="color:darkblue;"> id:</p></label>
+			<input type = "text" name = "id" id = "id" />
+            <label><p style="color:darkblue;"> room:</p></label>
+			<input type = "text" name = "room" id = "room" />
+            <label><p style="color:darkblue;"> Check-in day:</p></label>
+			<input type = "text" name = "Check-in day" id = "Check-in day" />
+            <label><p style="color:darkblue;"> Check-out day:</p></label>
+			<input type = "text" name = "Check-out day" id = "Check-out day" />
+            </form>
+            <input type = "submit" value ="Submit"/>
+            <br />
+         </form>
         <div class="side-nav">
             <div class="logo">
                 <i class="fas fa-tachometer-alt"></i>
